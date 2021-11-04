11b99cac9c7d952053dd3a09b222f3e695a08b17
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-11-04 14:14:47 +0500

    refactor: var naming

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,18 @@
 const lettersLength = 26;
 const upperCharCodeStart = 64;
-const upperCharCodeEnd = 90;
+const lastUpperCharCode = 90;
 const lowerCharCodeStart = 96;
 const lastLowerCharCode = 122;
 
 const isUpperCaseLetter = (letter) =>
-  upperCharCodeStart < letter.charCodeAt() && letter.charCodeAt() <= upperCharCodeEnd;
+  upperCharCodeStart < letter.charCodeAt() && letter.charCodeAt() <= lastUpperCharCode;
 const isLowerCaseLetter = (letter) =>
   lowerCharCodeStart < letter.charCodeAt() && letter.charCodeAt() <= lastLowerCharCode;
 
 module.exports = {
   upperCharCodeStart,
   lowerCharCodeStart,
-  upperCharCodeEnd,
+  lastUpperCharCode,
   lastLowerCharCode,
   isLowerCaseLetter,
   isUpperCaseLetter,
