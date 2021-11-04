9346444bdccbe2b97e0c9df70ef0af66d2f6bf17
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-11-04 14:26:28 +0500

    fix: ROT-8 cipher to enc&dec

--- src/ROT8Cipher.js
+++ src/ROT8Cipher.js
@@ -1,24 +1,30 @@
 const { isLowerCaseLetter, isUpperCaseLetter } = require('./utils');
 
-const ROT8Cipher = (input) => {
+const ROT8Cipher = (input, flag) => {
   const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const upperAlphabetPropelled = 'IJKLMNOPQRSTUVWXYZABCDEFGH';
   const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
   const lowerAlphabetPropelled = 'ijklmnopqrstuvwxyzabcdefgh';
 
+  // like decryption(0) or encryption(1)
+
   return input
     .split('')
     .map((character) => {
       if (isUpperCaseLetter(character))
-        return upperAlphabetPropelled[upperAlphabet.indexOf(character)];
+        return flag
+          ? upperAlphabet[upperAlphabetPropelled.indexOf(character)]
+          : upperAlphabetPropelled[upperAlphabet.indexOf(character)];
       else if (isLowerCaseLetter(character))
-        return lowerAlphabetPropelled[lowerAlphabet.indexOf(character)];
+        return flag
+          ? lowerAlphabet[lowerAlphabetPropelled.indexOf(character)]
+          : lowerAlphabetPropelled[lowerAlphabet.indexOf(character)];
 
       return character;
     })
     .join('');
 };
 
-console.log(ROT8Cipher('IJKLMNOPQRSTUVWXYZABCDEFGH'));
+console.log(ROT8Cipher('stuvwxyzabcdefghijklmnopqr', 0));
 
 module.exports = ROT8Cipher;
