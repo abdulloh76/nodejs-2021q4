0ed688f58cf5128df5f15b5e3fe8cbf59d35b786
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-11-08 18:49:44 +0500

    refactor: make closure to remember flag

--- src/ROT8Cipher.js
+++ src/ROT8Cipher.js
@@ -1,6 +1,6 @@
 const { isLowerCaseLetter, isUpperCaseLetter } = require('./utils');
 
-const ROT8Cipher = (input, flag) => {
+const ROT8Cipher = (flag) => {
   const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const upperAlphabetPropelled = 'IJKLMNOPQRSTUVWXYZABCDEFGH';
   const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
@@ -8,21 +8,22 @@ const ROT8Cipher = (input, flag) => {
 
   // like decryption(0) or encryption(1)
 
-  return input
-    .split('')
-    .map((character) => {
-      if (isUpperCaseLetter(character))
-        return flag
-          ? upperAlphabet[upperAlphabetPropelled.indexOf(character)]
-          : upperAlphabetPropelled[upperAlphabet.indexOf(character)];
-      else if (isLowerCaseLetter(character))
-        return flag
-          ? lowerAlphabet[lowerAlphabetPropelled.indexOf(character)]
-          : lowerAlphabetPropelled[lowerAlphabet.indexOf(character)];
+  return (input) =>
+    input
+      .split('')
+      .map((character) => {
+        if (isUpperCaseLetter(character))
+          return flag
+            ? upperAlphabet[upperAlphabetPropelled.indexOf(character)]
+            : upperAlphabetPropelled[upperAlphabet.indexOf(character)];
+        else if (isLowerCaseLetter(character))
+          return flag
+            ? lowerAlphabet[lowerAlphabetPropelled.indexOf(character)]
+            : lowerAlphabetPropelled[lowerAlphabet.indexOf(character)];
 
-      return character;
-    })
-    .join('');
+        return character;
+      })
+      .join('');
 };
 
 module.exports = ROT8Cipher;
--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -8,27 +8,32 @@ const {
   lastLowerCharCode,
 } = require('./utils');
 
-const caesarCipher = (input, shift = 1, flag) => {
+const caesarCipher = (flag, shift = 1) => {
   const chooser = flag ? -1 : 1; // like decryption(0) or encryption(1)
 
   // E(x) = (x + shift) % lettersLength
   // D(x) = (x - shift) % lettersLength
 
-  return input
-    .split('')
-    .map((character) => {
-      if (isUpperCaseLetter(character)) {
-        const move =
-          (character.charCodeAt() - upperCharCodeStart + shift * chooser) % lettersLength;
-        return String.fromCharCode(move > 0 ? move + upperCharCodeStart : move + lastUpperCharCode);
-      } else if (isLowerCaseLetter(character)) {
-        const move =
-          (character.charCodeAt() - lowerCharCodeStart + shift * chooser) % lettersLength;
-        return String.fromCharCode(move > 0 ? move + lowerCharCodeStart : move + lastLowerCharCode);
-      }
-      return character;
-    })
-    .join('');
+  return (input) =>
+    input
+      .split('')
+      .map((character) => {
+        if (isUpperCaseLetter(character)) {
+          const move =
+            (character.charCodeAt() - upperCharCodeStart + shift * chooser) % lettersLength;
+          return String.fromCharCode(
+            move > 0 ? move + upperCharCodeStart : move + lastUpperCharCode
+          );
+        } else if (isLowerCaseLetter(character)) {
+          const move =
+            (character.charCodeAt() - lowerCharCodeStart + shift * chooser) % lettersLength;
+          return String.fromCharCode(
+            move > 0 ? move + lowerCharCodeStart : move + lastLowerCharCode
+          );
+        }
+        return character;
+      })
+      .join('');
 };
 
 module.exports = caesarCipher;
